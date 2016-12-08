af0920537bacbbe4ac81ad90943b8453c53064dc
kal667 <kkelly.667@gmail.com>
2016-12-08 11:30:16 -0500

    Virtual access now gets you to the date that you need. The openxc script does...nothing? Need to write more and test.

--- virtual_access.py
+++ virtual_access.py
@@ -1,13 +1,28 @@
-    from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 import urllib
 import urllib2
 
-x=raw_input("google.com")
-refreshrate=raw_input("10")
-refreshrate=int(refreshrate)
-driver = webdriver.Firefox()
-driver.get("http://"+x)
+refreshrate=int(10)
+driver = webdriver.Chrome()
+driver.get("http://umd-openxc.azurewebsites.net/devices/352682050225977/data")
+
+username = driver.find_element_by_id("UserName")
+password = driver.find_element_by_id("Password")
+
+username.send_keys("OpenXCAdmin")
+password.send_keys("VXdDaBvdDU29rofs4Bmg")
+
+xpath = '//input[@type="submit"]'
+driver.find_element_by_xpath(xpath).click()
+
+raw_input("Select date. Then press 'enter' to continue ...")
+
+xpath = '//button[@type="submit"]'
+driver.find_element_by_xpath(xpath).click()
+
 while True:
     time.sleep(refreshrate)
+    print('Refresh')
     driver.refresh()
