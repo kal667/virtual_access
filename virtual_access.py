85e11a38cfa600eee3112a06ee09f9cd1a9a6014
kal667 <kkelly.667@gmail.com>
2016-12-09 14:48:21 -0500

    This adds a timer and prints the timestamp of the message if the timer has passed its relative time. Only reads in meassages with timestamps greater than that of the last message read in.

--- virtual_access.py
+++ virtual_access.py
@@ -5,6 +5,8 @@ import time
 import urllib
 import urllib2
 import json
+from datetime import datetime
+import calendar
 
 refreshrate=int(30)
 driver = webdriver.Chrome()
@@ -42,31 +44,48 @@ CAN_data.append([])
 CAN_data.append([])
 CAN_data.append([])
 
+#Initialize 
+base_timestamp = 0;
+last_timestamp = 0
+message_count = 0
+#Gets current UTC time and converts to timestamp
+base_time = datetime.utcnow()
+base_time = calendar.timegm(base_time.utctimetuple())
+
 while True:
 	#Get table rows
 	xpath = '//table[@id="loggedDataTable"]/tbody/tr'
 	rows = driver.find_elements_by_xpath(xpath)
 	rowCount = len(rows)
-	print 'Row count = ', rowCount
 
-	#Reading from bottom to top because new data loads to top
-	datum = 0
-	for i in reversed(range(rowCount)):
-		#print rows[i].text
-		header, data = rows[i].text.split("M",1)
-		#print header
-		#print data
+	#Reading from bottom to top because new data loads at top
+	for row in reversed(range(rowCount)):
+		header, data = rows[row].text.split("M",1)
 		parsed = json.loads(data)
 		#Reads timestamp for every record in the row
 		for each in parsed['records']:
-			CAN_data[0].append(each['timestamp'])
-			print(each['timestamp'])
-			datum += 1
-	print 'Datum = ', datum
-	print CAN_data
-
+			#Assigns base timestamp
+			if base_timestamp == 0:
+				base_timestamp = int(each['timestamp'])
+			#Reads
+			if int(each['timestamp']) > last_timestamp:
+				CAN_data[0].append(each['timestamp'])
+				last_timestamp = int(each['timestamp'])
+				message_count += 1
+	
+	print 'Message Count = ', message_count
+	
+	#Prints timestamp if time is greater than relative time 
+	while len(CAN_data[0]) != 0:
+		current_time = datetime.utcnow()
+		current_time = calendar.timegm(current_time.utctimetuple())
+		if (current_time - base_time) > ((CAN_data[0][0] - base_timestamp)/1000):
+			print CAN_data[0][0]
+			CAN_data[0].pop(0)
+			message_count -= 1
 
 	#Refresh
+	print 'Sleep'
 	time.sleep(refreshrate)
 	print 'Refresh'
 	driver.refresh()
