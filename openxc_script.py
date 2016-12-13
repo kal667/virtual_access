464d598ef8497f3ae2dbb75202b7a18bf79b7f77
kal667 <kkelly.667@gmail.com>
2016-12-13 14:28:52 -0500

    This cleans up the code and prepares for VI write capabilities. Needs to be tested once data is put on the server.

--- openxc_script.py
+++ openxc_script.py
@@ -1,10 +1,15 @@
 from openxc.interface import UsbVehicleInterface
 
-def receive(message, **kwargs):
-    # this callback will receive each message received as a dict
-    print(message['name'])
-
-vi = UsbVehicleInterface(callback=receive)
-vi.start()
-# This will block until the connection dies or you exit the program
-vi.join()
+def write_CAN_message(CAN_data, base_timestamp):
+    # this writes a low-level CAN message to the bus
+    while len(CAN_data[0]) != 0:
+		current_time = datetime.utcnow()
+		current_time = calendar.timegm(current_time.utctimetuple())
+		#Note: must divide OpenXC timestamp by 1000 because it includes ms
+		if (current_time - base_time) > ((CAN_data[0][0] - base_timestamp)/1000):
+			vi.write(bus=CAN_data[1][0], id=CAN_data[2][0], data=CAN_data[3][0])
+			CAN_data[0].pop(0)
+			CAN_data[1].pop(0)
+			CAN_data[2].pop(0)
+			CAN_data[3].pop(0)
+			
--- virtual_access.py
+++ virtual_access.py
@@ -1,59 +1,52 @@
 #!/usr/local/bin/python
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from datetime import datetime
+from openxc_script import write_CAN_message
 import time
 import urllib
 import urllib2
 import json
-from datetime import datetime
 import calendar
 
-refreshrate=int(30)
-driver = webdriver.Chrome()
-driver.get("http://umd-openxc.azurewebsites.net/devices/352682050225977/data")
+def navigate_to_data(driver):
+	#Opens browser and queries data on server
 
-username = driver.find_element_by_id("UserName")
-password = driver.find_element_by_id("Password")
+	#Chrome browser navigates to azure site
+	driver.get("http://umd-openxc.azurewebsites.net/devices/352682050225977/data")
 
-username.send_keys("OpenXCAdmin")
-password.send_keys("VXdDaBvdDU29rofs4Bmg")
+	username = driver.find_element_by_id("UserName")
+	password = driver.find_element_by_id("Password")
 
-#Clicks Login
-xpath = '//input[@type="submit"]'
-driver.find_element_by_xpath(xpath).click()
+	#Enters username and password
+	username.send_keys("OpenXCAdmin")
+	password.send_keys("VXdDaBvdDU29rofs4Bmg")
 
-#User sets date in browser for data to simulate
-raw_input("Select date. Then press 'enter' to continue ...")
+	#Clicks Login
+	xpath = '//input[@type="submit"]'
+	driver.find_element_by_xpath(xpath).click()
 
-#Query data at that date
-xpath = '//button[@type="submit"]'
-driver.find_element_by_xpath(xpath).click()
+	#User sets date in browser for data to simulate
+	raw_input("Select date. Then press 'enter' to continue ...")
 
-#Display 100 elements
-xpath = '//select[@name="loggedDataTable_length"]//option[@value="100"]'
-driver.find_element_by_xpath(xpath).click()
+	#Query data at that date
+	xpath = '//button[@type="submit"]'
+	driver.find_element_by_xpath(xpath).click()
 
-#Sort newest first
-xpath = '//th[@class="sorting_asc"]'
-driver.find_element_by_xpath(xpath).click()
+	#Display 100 elements
+	xpath = '//select[@name="loggedDataTable_length"]//option[@value="100"]'
+	driver.find_element_by_xpath(xpath).click()
 
-#Create array to hold CAN messages: timestamp / bus / id / data
-CAN_data = []
-CAN_data.append([])
-CAN_data.append([])
-CAN_data.append([])
-CAN_data.append([])
+	#Sort newest first
+	xpath = '//th[@class="sorting_asc"]'
+	driver.find_element_by_xpath(xpath).click()
 
-#Initialize 
-base_timestamp = 0;
-last_timestamp = 0
-message_count = 0
-#Gets current UTC time and converts to timestamp
-base_time = datetime.utcnow()
-base_time = calendar.timegm(base_time.utctimetuple())
+	return;
 
-while True:
-	#Get table rows
+def get_messages(driver, CAN_data, base_timestamp, last_timestamp, message_count):
+	#Read all CAN messages into the CAN_data array
+	
+	#Get message table rows
 	xpath = '//table[@id="loggedDataTable"]/tbody/tr'
 	rows = driver.find_elements_by_xpath(xpath)
 	rowCount = len(rows)
@@ -70,22 +63,48 @@ while True:
 			#Reads
 			if int(each['timestamp']) > last_timestamp:
 				CAN_data[0].append(each['timestamp'])
+				#CAN_data[1].append(each['bus'])
+				#CAN_data[2].append(each['id'])
+				#CAN_data[3].append(each['data'])
 				last_timestamp = int(each['timestamp'])
 				message_count += 1
-	
-	print 'Message Count = ', message_count
-	
-	#Prints timestamp if time is greater than relative time 
-	while len(CAN_data[0]) != 0:
-		current_time = datetime.utcnow()
-		current_time = calendar.timegm(current_time.utctimetuple())
-		if (current_time - base_time) > ((CAN_data[0][0] - base_timestamp)/1000):
-			print CAN_data[0][0]
-			CAN_data[0].pop(0)
-			message_count -= 1
 
-	#Refresh
-	print 'Sleep'
-	time.sleep(refreshrate)
-	print 'Refresh'
-	driver.refresh()
+	return base_timestamp, last_timestamp, message_count;
+
+
+def main():
+
+	#Create array to hold CAN messages: timestamp / bus / id / data
+	CAN_data = []
+	for i in range(4):
+		CAN_data.append([])
+
+	#Initialize 
+	driver = webdriver.Chrome()
+	base_timestamp = 0;
+	last_timestamp = 0
+	message_count = 0
+
+	#Gets current UTC time and converts to timestamp
+	base_time = datetime.utcnow()
+	base_time = calendar.timegm(base_time.utctimetuple())
+
+	#set refreshrate if applicable
+	refreshrate = int(5)
+    
+	navigate_to_data(driver)
+	while True:
+		base_timestamp, last_timestamp, message_count = get_messages(driver, CAN_data, base_timestamp, last_timestamp, message_count)
+		print 'Message Count = ', message_count
+		print 'Base = ', base_timestamp
+		print 'Last = ', last_timestamp
+		print CAN_data
+		#write_CAN_message(CAN_data, base_timestamp)
+		#Refresh
+		print 'Sleep'
+		time.sleep(refreshrate)
+		print 'Refresh'
+		driver.refresh()
+
+if __name__ == "__main__":
+    main()
